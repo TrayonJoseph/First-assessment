252438efbe9f407906e29f9c1a7c71786226eb1f
COR Students <corstudents@Matts-MBP-69.localdomain>
2018-10-22 12:10:25 -0400

    First Assessment

---
+++ assessment.js
@@ -0,0 +1,71 @@
+/* Question 1: Create a variable of each of the following data types 
+               String, Int, Bool, Float
+
+    Question 2: Create a variable called trueOFalse. Create an if 
+                statment so that if the variable is true the number 1 is
+                returned. Otherwise the number 2 is returned.
+    
+    Question 3: Create a for loop that counts every even number from
+                1-100 starting at 0
+
+     Question 4: Create a variable that is a bool and is equal to true
+                 Create a while loop that will print "hello"
+                 to the console 20 times and then stop.
+
+    Question 5: Create a function that that can determine if a parameter
+                is true or false. If it is true return the string "true".
+                Otherwise, return the string "false"
+    
+    Bonus Question: What does D.R.Y stand for?
+
+    */
+
+
+
+// //Question1
+// let name = "Chiheed";
+// let num = 24;
+// let car = true;
+// let pi = 3.14;
+
+// //Question 2
+// let trueOrFalse = false;
+// if(trueOrFalse === false){
+//     return(1)
+// } else{
+//     trueOrFalse=== true
+//     return(2)
+// }
+
+// //Question3
+// for(i=0;1<100;i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+//Question 4
+let purple = true
+let counter = 0
+while(purple === true){
+    console.log("Hello")
+    if(counter <= 20){
+      counter ++
+      purple = false
+    }
+    
+} 
+
+
+// //Question 5
+
+// function cars(paint){
+//     if(paint === true){
+//         return "true"
+//     } else {
+//         return "false"
+//     }
+// }
+
+//Question 6
+// Don't repeat yourself
